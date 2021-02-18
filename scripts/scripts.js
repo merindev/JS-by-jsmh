13f22a76a693bb5936c7b64e3539c2a5ae6aaa92
merindev <merindev@outlook.com>
2021-02-18 23:57:31 +0600

    Completed chapter 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,3 +9,22 @@ let person = {
     age: 26
 }
 console.log(typeof person);
+
+let typeofVariable = {
+    boolean: true,
+    string: "Head",
+    number: 25
+};
+
+console.log(typeof typeofVariable.boolean)
+
+let dynamicArray =["Merin","Jerin","Serin"]
+dynamicArray[3] = "Brothers in arms"
+console.log(dynamicArray)
+
+//Javascript functions
+
+function greetUser(firstName,lastName){
+    console.log(firstName + " " + lastName)
+}
+greetUser("Risalat Hasan","Merin")
